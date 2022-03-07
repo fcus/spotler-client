a0d3075b7e8b322754545cc398b6daaf93b6a3b7
Anne Jouke van den Berg <annejouke@protonmail.com>
2022-03-08 04:58:22 +0800

    sc-1: pass querystring in url

--- src/shared/base/spotler-client-base.ts
+++ src/shared/base/spotler-client-base.ts
@@ -26,11 +26,14 @@ export abstract class SpotlerClientBase {
 
     private async request(args: SpotlerClientRequestArgs) {
         return new Promise((resolve, reject) => {
+            const queryString = args.query
+                ? `?${QS.stringify(args.query)}`
+                : '';
+
             const oauthData = OAuth.authorize(
                 {
                     method: args.method,
-                    url: `https://${this.baseUrl}/${this.apiPath}/${args.endpoint}`,
-                    query: args.query,
+                    url: `https://${this.baseUrl}/${this.apiPath}/${args.endpoint}${queryString}`,
                 },
                 {
                     consumer: {
@@ -40,8 +43,6 @@ export abstract class SpotlerClientBase {
                 },
             );
 
-            const query = args.query ? `?${QS.stringify(args.query)}` : '';
-
             const options: RequestOptions = {
                 headers: {
                     Accept: 'application/json',
@@ -49,7 +50,7 @@ export abstract class SpotlerClientBase {
                 },
                 hostname: this.baseUrl,
                 method: args.method,
-                path: `/${this.apiPath}/${args.endpoint}${query}`,
+                path: `/${this.apiPath}/${args.endpoint}${queryString}`,
                 port: 443,
             };
 
