140b7c7553e4962741a8165627559a31942a5059
Anne Jouke van den Berg <annejouke@protonmail.com>
2022-03-31 15:25:23 +0800

    main: patch 0.1.3 enable prepublish

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export * from './lib/campaign/spotler-campaign.repository';
 export * from './lib/contact/spotler-contact.repository';
+export * from './model/spotler-campaign';
 export * from './model/spotler-contact';
 export * from './shared/config/spotler-config';
 export * from './shared/http-method.type';
